b6d1c57d55fa7f54c2286268f2fe52c00d9fbdde
Vladimir Grebenschikov <vova@odin.com>
2016-01-16 11:07:42 +0300

    - fix problem with memory locations above 7 - simplify code of memory locations mapping

--- bundles/binding/org.openhab.binding.plclogo/src/main/java/org/openhab/binding/plclogo/internal/PLCLogoMemMap.java
+++ bundles/binding/org.openhab.binding.plclogo/src/main/java/org/openhab/binding/plclogo/internal/PLCLogoMemMap.java
@@ -1,5 +1,7 @@
 package org.openhab.binding.plclogo.internal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /*******************
  * Utility Class to help with memory mapping
@@ -10,44 +12,63 @@ package org.openhab.binding.plclogo.internal;
 public class PLCLogoMemMap {
 	private int[] retval = new int[2]; // bit location[0] and real mem loc[1]
 
+	private static final Logger logger =
+			LoggerFactory.getLogger(PLCLogoBinding.class);
+
 	public int[] convertToReal(String memloc) {
 		retval[0] = -1;
 		retval[1] = -1;
 		// I , Q and M have bit values derived: I1 is equivalent to VB923.0
 		// TODO Add some validation to input parameters!
 		
-		if ((memloc.substring(0,2).equalsIgnoreCase("VB"))||(memloc.substring(0,2).equalsIgnoreCase("VW"))){
-			retval[1] = Integer.parseInt(memloc.substring(2, memloc.length()));
-			
+		if (memloc.length() < 2)
+			return null;
+
+		String mt; // normalizaed memory type VB|VW|I|Q|M|AO|AQ|AM
+		int idx;   // normalized index of element (starting from 0), for all but VB|VW
+		if (Character.isDigit(memloc.charAt(1)))
+		{
+			mt = memloc.substring(0,1).toUpperCase();
+			idx = Integer.parseInt(memloc.substring(1)) - 1;
 		}
-		if (memloc.substring(0,1).equalsIgnoreCase("I")){
-			// I starts at 923 for three bytes
-			retval[1]= 923 + ((Integer.parseInt(memloc.substring(1, memloc.length()))-1)/8);
-			retval[0] = (Integer.parseInt(memloc.substring(1, memloc.length()))%8-1);
-		}
-		if (memloc.substring(0,1).equalsIgnoreCase("Q")){
-			// Q starts at 942 for two bytes
-			retval[1]=  942 + ((Integer.parseInt(memloc.substring(1, memloc.length()))-1)/8);
-			retval[0] = (Integer.parseInt(memloc.substring(1, memloc.length()))%8-1);
-		}
-		if (memloc.substring(0,1).equalsIgnoreCase("M")){
-			// Markers starts at 948 for two bytes
-			retval[1]= 948 + ((Integer.parseInt(memloc.substring(1, memloc.length()))-1)/8);
-			retval[0] = ((Integer.parseInt(memloc.substring(1, memloc.length()))%8)-1);
-		}
-		if (memloc.substring(0,2).equalsIgnoreCase("AI")){
-			// AI starts at 926 for 8 words
-			retval[1]=  926 + ((Integer.parseInt(memloc.substring(2, memloc.length()))-1)*2);
-		}
-		if (memloc.substring(0,2).equalsIgnoreCase("AQ")){
-			// AQ starts at 944 for 2 words
-			retval[1]=  944 + ((Integer.parseInt(memloc.substring(2, memloc.length()))-1)*2);
-		}
-		if (memloc.substring(0,2).equalsIgnoreCase("AM")){
-			// AM starts at 952 for 16 words
-			retval[1]=  952 + ((Integer.parseInt(memloc.substring(2, memloc.length()))-1)*2);
+		else
+		{
+			mt = memloc.substring(0,2).toUpperCase();
+			idx = Integer.parseInt(memloc.substring(2)) - 1;
 		}
 
+		if (mt.equals("VB") || mt.equals("VW")) {
+			retval[1] = Integer.parseInt(memloc.substring(2));
+		} else
+		if (mt.equals("I")) {
+			// I starts at 923 for three bytes
+			retval[1] = 923 + idx/8;
+			retval[0] = idx%8;
+		} else
+		if (mt.equals("Q")) {
+			// Q starts at 942 for two bytes
+			retval[1] = 942 + idx/8;
+			retval[0] = idx%8;
+		} else
+		if (mt.equals("M")) {
+			// Markers starts at 948 for two bytes
+			retval[1] = 948 + idx/8;
+			retval[0] = idx%8;
+		} else
+		if (mt.equals("AI")) {
+			// AI starts at 926 for 8 words
+			retval[1] =  926 + idx*2;
+		} else
+		if (mt.equals("AQ")) {
+			// AQ starts at 944 for 2 words
+			retval[1] =  944 + idx*2;
+		} else
+		if (mt.equals("AM")) {
+			// AM starts at 952 for 16 words
+			retval[1] =  952 + idx*2;
+		}
+
+		logger.debug("Memory map for " + memloc + " = " + retval[1] + ((retval[0] != -1)?("." + retval[0]):""));
 		return retval;
 	}
 
